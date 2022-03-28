0ca46b5783d665d90985039fa53bd76be13b3650
Nikolay Mihaylov <nikolay@insurify.com>
2022-03-28 16:37:03 +0300

    custom filed renderer option

--- packages/core/admin/admin/src/content-manager/components/Inputs/index.js
+++ packages/core/admin/admin/src/content-manager/components/Inputs/index.js
@@ -245,7 +245,7 @@ function Inputs({
       placeholder={placeholder ? { id: placeholder, defaultMessage: placeholder } : null}
       required={fieldSchema.required || false}
       step={step}
-      type={inputType}
+      type={fieldSchema.customFieldConfig?.fieldRenderer || inputType}
       // validations={validations}
       value={inputValue}
       withDefaultValue={false}
